356a30e5e74e525fca2c7deb5515fcb7d86bc88b
nukeop <12746779+nukeop@users.noreply.github.com>
2025-04-07 13:50:50 +0200

    Fix build

--- src/components/FloorplanViewer.tsx
+++ src/components/FloorplanViewer.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 
-type FloorplanViewerProps = {
-  // Define props here as you develop the component
-};
-
-export default function FloorplanViewer(_props: FloorplanViewerProps) {
+export default function FloorplanViewer() {
   return (
     <div className="h-full w-full">
-      {/* This component will be developed later */}
       <div className="flex h-full w-full items-center justify-center rounded border-2 border-dashed border-gray-300 bg-gray-50">
         <p className="text-gray-500">Floorplan content will go here</p>
       </div>
