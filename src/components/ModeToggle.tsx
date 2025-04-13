ec1634ef50df7aea899d2b2445329b2b5134b122
nukeop <12746779+nukeop@users.noreply.github.com>
2025-04-13 19:17:39 +0200

    Fix routing issue

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -2,19 +2,20 @@
 import React from 'react';
 import { FaEye, FaEdit } from 'react-icons/fa';
 import { useFloorplan } from '@/contexts/FloorplanContext';
+import Link from 'next/link';
 
 export function ModeToggle() {
   const { isEditorMode } = useFloorplan();
 
   return (
     <div className="absolute top-4 right-4 z-10">
-      <a
+      <Link
         href={isEditorMode ? '/' : '/editor'}
         className="p-2 bg-blue-500 text-white rounded shadow hover:bg-blue-600 focus:outline-none flex items-center justify-center"
         title={isEditorMode ? "Switch to Viewer Mode" : "Switch to Editor Mode"}
       >
         {isEditorMode ? <FaEye className="w-4 h-4" /> : <FaEdit className="w-4 h-4" />}
-      </a>
+      </Link>
     </div>
   );
 }
