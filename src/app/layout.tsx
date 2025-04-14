1c0c50461850afdfc2d690ce08240b08770e3287
nukeop <12746779+nukeop@users.noreply.github.com>
2025-04-14 11:23:40 +0200

    Add Umami analytics script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async defer data-website-id="55a0087a-46b1-49c8-81c5-576fe532fa6d" src="https://umami.gumblert.tech/umami.js"></script>
+      </head>
       <body className={inter.className}>
         <div className="min-h-screen bg-gray-50">
           <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
